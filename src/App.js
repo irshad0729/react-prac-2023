701fb3c7654f878c19059b020e07f451f8b1a4b2
cdeveloper123 <cdeveloper2016@gmail.com>
2023-09-19 00:00:40 +0530

    feat(home-page): Add mui button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [jsonData, setJsonData] = useState();
+  const top100Films = [
+    { label: "The Shawshank Redemption", year: 1994 },
+    { label: "The Godfather", year: 1972 },
+    { label: "The Godfather: Part II", year: 1974 },
+    { label: "The Dark Knight", year: 2008 },
+    { label: "12 Angry Men", year: 1957 },
+    { label: "Schindler's List", year: 1993 },
+    { label: "Pulp Fiction", year: 1994 },
+  ];
+
+  useEffect(() => {
+    async function getData() {
+      const data = await fetch("https://jsonplaceholder.typicode.com/users");
+      const response = await data.json();
+      console.log(response);
+      setJsonData(response);
+    }
+    getData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Button variant="contained" color="success" startIcon={<SendIcon />}>
+        Contained
+      </Button>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={jsonData}
+        getOptionLabel={(jsonData) =>
+          `${jsonData.id} (${jsonData.username}) +${jsonData.name}`
+        }
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Movie" />}
+      />
     </div>
   );
 }
